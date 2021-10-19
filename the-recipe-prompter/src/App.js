146c30ce68b6f2d95bf44490152ff68f97e115da
MassimilianoMarcello <massimarcello@hotmail.it>
2021-10-20 00:10:08 +0200

    first update

--- the-recipe-prompter/src/App.js
+++ the-recipe-prompter/src/App.js
@@ -2,12 +2,16 @@ import logo from './logorecipe.svg';
 import './App.css';
 
 function App() {
+const APP_ID = "65268f1f";
+const APP_KEY="d7809ab1d3c1a8e99f1957eec0970e27";
+const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>The application that suggests</p>
-        <p>the dishes to be created</p>
+        <p>App suggesting dishes to create</p>
+   
       </header>
     </div>
   );
