90a3de40e41194662ecf4d221a3c365ee586084d
MassimilianoMarcello <massimarcello@hotmail.it>
2021-10-22 03:55:54 +0200

    last up

--- the-recipe-prompter/src/App.js
+++ the-recipe-prompter/src/App.js
@@ -3,21 +3,33 @@ import logo from "./logorecipe.svg";
 import "./App.css";
 import Axios from "axios";
 import{v4 as uuidv4}from'uuid';
+import alert from './components/Alert'
 import Recipe from './components/Recipe';
+import Alert from "./components/Alert";
 
 function App() {
   const [query, setQuery] = useState("");
 const[recipes,setRecipes]=useState([])
+const[alert,setAlert]= useState("")
   // data from edamam.com
   const APP_ID = "65268f1f";
   const APP_KEY = "d7809ab1d3c1a8e99f1957eec0970e27";
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
   // fetch api with axios
   const getData = async () => {
-    const result = await Axios.get(url);
-    setRecipes(result.data.hits)
-    console.log(result);
-    setQuery("");
+    if(query!==""){
+      const result = await Axios.get(url);
+      if(!result.data.more){
+        return setAlert("No food with such name")
+      }
+      setRecipes(result.data.hits);
+      console.log(result);
+      setAlert("")
+      setQuery("");
+    }else{
+      setAlert("Please fill in the form")
+    }
+    
   };
   // change the query
   const onChange=e=>{
@@ -36,6 +48,7 @@ const[recipes,setRecipes]=useState([])
         <img src={logo} className="App-logo" alt="logo" />
         <p>App suggesting dishes to create</p>
         <form className="search-form" onSubmit={onSubmit}>
+          {alert !== "" && <Alert alert={alert} />}
           <input
             type="text"
             placeholder="Enter Ingredient"
@@ -43,13 +56,12 @@ const[recipes,setRecipes]=useState([])
             onChange={onChange}
             value={query}
           />
-          <input type="submit" value="Search" />
+          <input type="submit" value="search" />
         </form>
       </header>
-      <article className='recipes'>
-{recipes !==[] && recipes.map (recipe=>
- <Recipe  key={uuidv4} recipe={recipe}/>
-  )}
+      <article className="recipes">
+        {recipes !== [] &&
+          recipes.map((recipe) => <Recipe key={uuidv4} recipe={recipe} />)}
       </article>
     </div>
   );
--- the-recipe-prompter/src/components/Recipe.js
+++ the-recipe-prompter/src/components/Recipe.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import RecipeInfo from './RecipeInfo';
 
 const Recipe = ({recipe}) => {
+    const[show,setShow]=useState(false)
     const{label,image,url,ingredients}= recipe.recipe;
     return (
-        <div className='recipe'>
-            <h2>{label}</h2>
-            <img src={image} alt={label} />
-            <a href={url} target='_blank' rel='noopener noreferrer'>Go to the recipe</a>
-            <button>Ingredients</button>
-        </div>
-    )
+      <div className="recipe">
+        <h2>{label}</h2>
+        <img src={image} alt={label} />
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          Go to the recipe
+        </a>
+        <button onClick={() => setShow(!show)}>Ingredients</button>
+        {show && <RecipeInfo ingredients={ingredients} />}
+      </div>
+    );
 }
 
 export default Recipe
